a46b8cc450cdbfa0c3952bdee15142c5e97da8d7
Anh Quan <quandang123654@gmail.com>
2024-04-25 15:24:07 +0700

    fix: fix bug wrong filter in shop view

--- src/app/components/user-pages/shop-view/shop-view.component.ts
+++ src/app/components/user-pages/shop-view/shop-view.component.ts
@@ -48,15 +48,17 @@ export class ShopViewComponent implements OnInit {
     private initialize() {
         this.isLoading = true;
 
-        this.productService.getAllProduct({page: this.page, size: this.size}).subscribe({
-            next: (res) => {
-                this.isLoading = false;
-                this.products = res;
-                this.sortedProd = _.cloneDeep(this.products);
-                this.defaultProd = _.cloneDeep(this.products);
-            },
-            error: (err) => console.log(err),
-        });
+        this.productService
+            .getAllProduct({ page: this.page, size: this.size })
+            .subscribe({
+                next: (res) => {
+                    this.isLoading = false;
+                    this.products = res;
+                    this.sortedProd = _.cloneDeep(this.products);
+                    this.defaultProd = _.cloneDeep(this.products);
+                },
+                error: (err) => console.log(err),
+            });
         forkJoin([
             this.productService.getCategory(),
             this.productService.getBrand(),
@@ -108,62 +110,87 @@ export class ShopViewComponent implements OnInit {
         dv.filter((event.target as HTMLInputElement).value, 'contains');
     }
 
-    onCheckboxBrandChange(event) {
-        this.products = [...this.defaultProd];
-        console.log(this.products);
-        console.log(event);
+    // onCheckboxBrandChange(event) {
+    //     this.products = [...this.defaultProd];
+    //     console.log(this.products);
+    //     console.log(event);
 
-        if (event.value === 'All') {
-            if (this.selectedCate === 'All') return;
-            else {
-                this.products = this.products.filter((prod) => {
-                    return (
-                        prod.subCategory.category.categoryId ===
-                        this.selectedCate
-                    );
-                });
-            }
-        } else {
-            if (this.selectedCate === 'All')
-                this.products = this.products.filter((prod) => {
-                    return prod.brand.brandId === event.value;
-                });
-            else
-                this.products = this.products.filter((prod) => {
-                    return (
-                        prod.brand.brandId === event.value &&
-                        prod.subCategory.category.categoryId ===
-                            this.selectedCate
-                    );
-                });
-        }
-        this.sortedProd = [...this.products];
-    }
+    //     if (event.value === 'All') {
+    //         if (this.selectedCate === 'All') return;
+    //         else {
+    //             this.products = this.products.filter((prod) => {
+    //                 return (
+    //                     prod.subCategory.category.categoryId ===
+    //                     this.selectedCate
+    //                 );
+    //             });
+    //         }
+    //     } else {
+    //         if (this.selectedCate === 'All')
+    //             this.products = this.products.filter((prod) => {
+    //                 return prod.brand.brandId === event.value;
+    //             });
+    //         else
+    //             this.products = this.products.filter((prod) => {
+    //                 return (
+    //                     prod.brand.brandId === event.value &&
+    //                     prod.subCategory.category.categoryId ===
+    //                         this.selectedCate
+    //                 );
+    //             });
+    //     }
+    //     this.sortedProd = [...this.products];
+    // }
 
-    onCheckboxCateChange(event) {
-        this.products = [...this.defaultProd];
-        if (event.value === 'All') {
-            if (this.selectedBrand === 'All') return;
-            else {
-                this.products = this.products.filter((prod) => {
-                    return prod.brand.brandId === this.selectedBrand;
-                });
-            }
-        } else {
-            if (this.selectedBrand === 'All')
-                this.products = this.products.filter((prod) => {
-                    return prod.subCategory.category.categoryId === event.value;
-                });
-            else
-                this.products = this.products.filter((prod) => {
-                    return (
-                        prod.subCategory.category.categoryId === event.value &&
-                        prod.brand.brandId === this.selectedBrand
-                    );
-                });
-        }
-        this.sortedProd = [...this.products];
-    }
+    // onCheckboxCateChange(event) {
+    //     this.products = [...this.defaultProd];
+    //     if (event.value === 'All') {
+    //         if (this.selectedBrand === 'All') return;
+    //         else {
+    //             this.products = this.products.filter((prod) => {
+    //                 return prod.brand.brandId === this.selectedBrand;
+    //             });
+    //         }
+    //     } else {
+    //         if (this.selectedBrand === 'All')
+    //             this.products = this.products.filter((prod) => {
+    //                 return prod.subCategory.category.categoryId === event.value;
+    //             });
+    //         else
+    //             this.products = this.products.filter((prod) => {
+    //                 return (
+    //                     prod.subCategory.category.categoryId === event.value &&
+    //                     prod.brand.brandId === this.selectedBrand
+    //                 );
+    //             });
+    //     }
+    //     this.sortedProd = [...this.products];
+    // }
 
     onPriceChange() {}
+
+    applyFilter() {
+        this.isLoading = true;
+        const filter = {
+            brandId: this.selectedBrand == 'All' ? null : this.selectedBrand,
+            categoryId: this.selectedCate == 'All' ? null : this.selectedCate,
+            minPrice: this.priceRange[0],
+            maxPrice: this.priceRange[1],
+            page: this.page,
+            size: this.size,
+        };
+        for (const key in filter) {
+            if (filter[key] === null) {
+                delete filter[key];
+            }
+        }
+        this.productService.getAllProduct({ ...filter }).subscribe({
+            next: (res) => {
+                this.isLoading = false;
+                this.products = res;
+            },
+            error: (err) => console.log(err),
+        });
+    }
+    clearFilter() {}
 }
