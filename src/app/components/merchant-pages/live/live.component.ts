32862385b702f56e86b80be2df69e817e80af716
PHoangLe <lehoangphuc180502@gmail.com>
2024-06-21 00:15:00 +0700

    fix: add ice server list

--- src/app/components/merchant-pages/live/live.component.ts
+++ src/app/components/merchant-pages/live/live.component.ts
@@ -118,12 +118,7 @@ export class LiveComponent extends BaseComponent implements OnInit, OnDestroy {
         this.OV.setAdvancedConfiguration({
             iceServers: [
                 {
-                    urls: [
-                        "turn:global.relay.metered.ca:80",
-                        "turn:global.relay.metered.ca:80?transport=tcp",
-                        "turn:global.relay.metered.ca:443",
-                        "turns:global.relay.metered.ca:443?transport=tcp"
-                    ],
+                    urls: "turns:global.relay.metered.ca:443",
                     username: "e5501e082a3b3e2c71bbd3e8",
                     credential: "a9RtoZ3qOO7qa8+/",
                 }
